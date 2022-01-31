4e1b5dacd249fb96dd2a3e2cbc0820bb3875f22a
Jorge Ochoa Reyes <jorgeochoareyes@gmail.com>
2022-01-31 13:15:43 -0800

    Version 1.0.2

--- client/src/Util/Util.js
+++ client/src/Util/Util.js
@@ -1,4 +1,4 @@
-const url_Deploy = "https://coderace.codes/";
+const url_Deploy = "https://nameless-plateau-19889.herokuapp.com/";
 // "https://nameless-plateau-19889.herokuapp.com/";
 // "https://coderace.codes/"
 // "http://localhost:3000";
